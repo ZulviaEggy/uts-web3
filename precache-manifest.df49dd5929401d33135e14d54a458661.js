5bc69ec647654cc3f0ab2debb1f133e7912d3470
zulviaeggy <zulviaeggy97@gmail.com>
2019-04-11 09:03:51 +0700

    Updates

---
+++ precache-manifest.df49dd5929401d33135e14d54a458661.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "43e737b9992baccc7f01",
+    "url": "/static/js/main.a585e56b.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "43e737b9992baccc7f01",
+    "url": "/static/css/main.fea2a284.chunk.css"
+  },
+  {
+    "revision": "4298e3778cbeff0c4a1a6ecc22053c2a",
+    "url": "/index.html"
+  }
+];
